6911e8cd84073ebe3747a064b3094d37ee644629
MrShevchuk <viacheslavshevchuk911@gmail.com>
2020-03-22 09:00:29 +0200

    Add three classes to github

---
+++ src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,8 @@
+package ua.zp.brainacad;
+
+public class ItemThree {
+    public static void main(String[] args) {
+        System.out.println("H\ne\nl\nl\no");
+        System.out.println("W\to\tr\tl\td");
+    }
+}
